70d22530502db984fe1b540f8c6e013c177dbf73
Drew <36288932+darkakito@users.noreply.github.com>
2018-02-08 22:49:26 -0700

    Add files via upload

--- GameFrame.java
+++ GameFrame.java
@@ -4,27 +4,40 @@ import java.awt.event.*;
 
 public class GameFrame extends JFrame implements ActionListener {
 
+  private JLabel diceLabel;
   private JButton diceButton;
+  private int move = 0;
 
   public GameFrame() {
-
     super("Mario Party");
     setSize(900, 800);
 
+    diceLabel = new JLabel("" + move);
     diceButton = new JButton("Roll Dice");
+    javax.swing.JTextField[] array = new javax.swing.JTextField[40];
 
     diceButton.addActionListener(this);
+
+    JPanel topPanel = new JPanel();
+    topPanel.add(diceLabel);
+
+    JPanel centerPanel = new JPanel();
+
+    JPanel bottomPanel = new JPanel();
+		bottomPanel.add(diceButton);
+
+    Container contentPane = getContentPane();
+		contentPane.add(topPanel, BorderLayout.NORTH);
+		contentPane.add(centerPanel, BorderLayout.CENTER);
+		contentPane.add(bottomPanel, BorderLayout.SOUTH);
 
     setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
   }
 
   public void actionPerformed(ActionEvent e) {
     if(e.getSource() == diceButton) {
-      public int dice (int diceRoll) {
-          diceRoll = (int)(Math.random()*6 ) + 1;
-          int move = diceRoll;
-          return move;
-      }
+      move = (int)(Math.random()*6 ) + 1;
+      diceLabel.setText("" + move);
     }
   }
 
