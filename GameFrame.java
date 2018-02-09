67a4eab08b08256cdae87122a255c832ed3c497c
Drew <36288932+darkakito@users.noreply.github.com>
2018-02-09 00:06:46 -0700

    Add files via upload

--- GameFrame.java
+++ GameFrame.java
@@ -10,21 +10,26 @@ import java.awt.event.*;
 
 public class GameFrame extends JFrame implements ActionListener {
 
-  private JLabel diceLabel, boardLabel;
+  private JLabel diceLabel, boardLabel, turnLabel;
   private JButton diceButton;
-  private int move = 0;
+  private int move = 0, turn = 0;
 
   public GameFrame() {
     super("Mario Party");
     setSize(800, 700);
+    getContentPane().setBackground(new java.awt.Color(255,255,255));
 
-    diceLabel = new JLabel("" + move);
+    diceLabel = new JLabel("Dice Roll:  ");
+    diceLabel.setFont(new Font("Serif", Font.BOLD, 18));
+    turnLabel = new JLabel("Turn:  ");
+    turnLabel.setFont(new Font("Serif", Font.BOLD, 18));
     diceButton = new JButton("Roll Dice");
 
     diceButton.addActionListener(this);
 
     JPanel topPanel = new JPanel();
     topPanel.add(diceLabel);
+    topPanel.add(turnLabel);
 
     JPanel centerPanel = new JPanel();
 
@@ -51,7 +56,9 @@ public class GameFrame extends JFrame implements ActionListener {
   public void actionPerformed(ActionEvent e) {
     if(e.getSource() == diceButton) {
       move = (int)(Math.random()*6 ) + 1;
-      diceLabel.setText("" + move);
+      diceLabel.setText("Dice Roll: " + move);
+      turn++;
+      turnLabel.setText("Turn: " + turn);
     }
   }
 
