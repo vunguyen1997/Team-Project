bb939a0e9e3c4ff895a6e55622e4a194f4bf1f49
Drew <36288932+darkakito@users.noreply.github.com>
2018-02-09 04:15:22 -0700

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Game {
+
+	private static int Players = 0;
+	private static int HumanPlayers = 0;
+	private static int AIplayers = 0;
+
+	public static void main(String[] args) {
+		int numPlayers = 0;
+		int numHumanPlayers = 0;
+		int numAIplayers = 0;
+		int enter =0;
+		boolean menuSet = false;
+		do {
+			try {
+				Scanner input = new Scanner(System.in);
+				System.out.println("How many Players?");
+				enter = input.nextInt();
+				if (enter <=1 || enter >4) {
+					System.out.println("Please enter a number between 2 and 4");
+					enter = input.nextInt();
+				}
+				else {
+					numPlayers = enter;
+				}
+
+				System.out.println("How many Human Players?");
+				enter = input.nextInt();
+				if (enter <=1 || enter >4) {
+					System.out.println("Please enter a number between 2 and 4");
+					enter = input.nextInt();
+				}
+				else {
+					numHumanPlayers = enter;
+					numAIplayers = numPlayers - numHumanPlayers;
+					menuSet = true;
+				}
+				}
+			catch(InputMismatchException exception) {
+				System.out.println("Please enter a valid number");
+			}
+			} while (menuSet = false);
+	}
+
+	public void setNumberofPlayers(int numPlayers) {
+			this.Players = numPlayers;
+		}
+
+	public int getNumberofPlayers(){
+		return Players;
+	}
+
+	public void setNumberofHumanplayers(int numHumanPlayers){
+		this.HumanPlayers = numHumanPlayers;
+	}
+
+	public int getNumberofHumanplayers(){
+		return HumanPlayers;
+	}
+	public void setNumberofAIplayers(int numAIplayers){
+		this.AIplayers = numAIplayers;
+	}
+
+	public int getNumberofAIplayers(){
+		return AIplayers;
+	}
+
+}
--- GameFrame.java
+++ GameFrame.java
@@ -15,21 +15,23 @@ import java.awt.event.*;
 
 public class GameFrame extends JFrame implements ActionListener {
 
-  private JLabel diceLabel, boardLabel, turnLabel;
+  private JLabel diceLabel, boardLabel, turnLabel, dicePLabel;
   private JButton diceButton, humanButton;
   private int move = 0, turn = 0;
 
   public GameFrame() {
     super("Mario Party");
-    setSize(800, 700);
+    setSize(1000, 600);
 
     diceLabel = new JLabel("Dice Roll:  ");
     diceLabel.setFont(new Font("Serif", Font.BOLD, 18));
+    dicePLabel = new JLabel();
+    dicePLabel.setIcon(new ImageIcon(getClass().getResource("dice0.png")));
+    dicePLabel.setSize(1, 1);
     turnLabel = new JLabel("Turn:  ");
     turnLabel.setFont(new Font("Serif", Font.BOLD, 18));
     diceButton = new JButton("Roll Dice");
     humanButton = new HumanPlayer();
-    humanButton.setLocation(400, 400);
 
     diceButton.addActionListener(this);
 
@@ -48,27 +50,32 @@ public class GameFrame extends JFrame implements ActionListener {
       e.printStackTrace();
     }
 
-    centerPanel.add(humanButton);
+    JPanel eastPanel = new JPanel();
+    eastPanel.add(dicePLabel);
 
     JPanel bottomPanel = new JPanel();
 		bottomPanel.add(diceButton);
 
     Container contentPane = getContentPane();
 		contentPane.add(topPanel, BorderLayout.NORTH);
-		contentPane.add(centerPanel, BorderLayout.CENTER);
+		contentPane.add(centerPanel, BorderLayout.WEST);
+    contentPane.add(eastPanel, BorderLayout.EAST);
 		contentPane.add(bottomPanel, BorderLayout.SOUTH);
 
     setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
   }
 
   public void actionPerformed(ActionEvent e) {
-    if(e.getSource() == diceButton) {
+    if (e.getSource() == diceButton) {
       move = (int)(Math.random()*6 ) + 1;
       diceLabel.setText("Dice Roll: " + move);
       turn++;
       turnLabel.setText("Turn: " + turn);
+      dicePLabel.setIcon(new ImageIcon(getClass().getResource("dice" + move + ".png")));
     }
   }
+
+// after every 4 turns, get a minigame prompt to show up
 
   public static void main(String[] args) {
     new GameFrame().setVisible(true);
